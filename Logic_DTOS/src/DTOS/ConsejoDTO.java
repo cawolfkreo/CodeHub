0af9c6c738f221b158f9276832b8b2a66d009f3e
cawolfkreo <camilozambranovotto@hotmail.com>
2017-02-10 08:37:11 -0500

    Paquete de Java y primeros DTOS :tada:
    agregué un módulo para Java y dentro de este (en la carpeta source) puse
    2 paquetes, el de los DTOs y el de la Logica. En el paquete de los DTOs
    se crearon las clases ConsejoDTO y EmergenciaDTO

---
+++ Logic_DTOS/src/DTOS/ConsejoDTO.java
@@ -0,0 +1,8 @@
+package DTOS;
+
+/**
+ * Clase que representa a un consejo básico.
+ * Created by c.zambrano10 on 10/02/2017.
+ */
+public class ConsejoDTO {
+}
---
+++ Logic_DTOS/src/DTOS/EmergenciaDTO.java
@@ -0,0 +1,8 @@
+package DTOS;
+
+/**
+ * Clase que representa la llegada de una emergencia básica
+ * Created by c.zambrano10 on 10/02/2017.
+ */
+public class EmergenciaDTO {
+}
