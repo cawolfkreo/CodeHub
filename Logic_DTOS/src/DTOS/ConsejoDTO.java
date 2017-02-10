9bb08dd42b313ab1907785ad0a68a4205db5e972
cawolfkreo <camilozambranovotto@hotmail.com>
2017-02-10 09:04:46 -0500

    Agregados atributos y constructores de los DTOs simples
    creo que necesito unos IDs en los atributos de mis DTOs :rage4:

--- Logic_DTOS/src/DTOS/ConsejoDTO.java
+++ Logic_DTOS/src/DTOS/ConsejoDTO.java
@@ -1,8 +1,68 @@
 package DTOS;
 
+import java.util.Date;
+
 /**
  * Clase que representa a un consejo básico.
  * Created by c.zambrano10 on 10/02/2017.
  */
 public class ConsejoDTO {
+    //--------------------------------------------------------------
+    //                          ATRIBUTOS
+    //--------------------------------------------------------------
+
+    /**
+     * Fecha de creacion de un consejo.*
+     */
+    private Date fecha;
+
+    /**
+     * mensaje del consejo.
+     */
+    private String mensaje;
+
+    /**
+     * dieta recomendada en el consejo.
+     */
+    private String dieta;
+
+    /**
+     * Actividad recomendada en el consejo.
+     */
+    private String actividadFisica;
+
+    /**
+     * Medicamentos recomendados en el consejo.
+     */
+    private String medicamentos;
+
+    //--------------------------------------------------------------
+    //                          Constructores
+    //--------------------------------------------------------------
+
+    /**
+     * constructor vacio
+     */
+    public ConsejoDTO (){
+
+    }
+
+    /**
+     * Constructor con todos los atributos de la clase.
+     * @param fecha
+     * @param mensaje
+     * @param dieta
+     * @param actividadFisica
+     * @param medicamentos
+     */
+    public ConsejoDTO(Date fecha, String mensaje, String dieta, String actividadFisica, String medicamentos) {
+        this.fecha = fecha;
+        this.mensaje = mensaje;
+        this.dieta = dieta;
+        this.actividadFisica = actividadFisica;
+        this.medicamentos = medicamentos;
+    }
+
+
+
 }
--- Logic_DTOS/src/DTOS/EmergenciaDTO.java
+++ Logic_DTOS/src/DTOS/EmergenciaDTO.java
@@ -1,8 +1,58 @@
 package DTOS;
 
+import java.util.Date;
+
 /**
  * Clase que representa la llegada de una emergencia básica
  * Created by c.zambrano10 on 10/02/2017.
  */
 public class EmergenciaDTO {
+
+    //--------------------------------------------------------------
+    //                          ATRIBUTOS
+    //--------------------------------------------------------------
+
+    /**
+     * Fecha de creacion de la emergencia.
+     */
+    private Date fecha;
+
+    /**
+     * estado de la emergencia.
+     */
+    private String estado;
+
+    /**
+     * la latitud de la ubicacion del paciente en el momento de la emergencia.
+     */
+    private int ubicacion_latitud;
+
+    /**
+     * la longitud de la ubicacion del paciente en el momento de la emergencia.
+     */
+    private int ubicacion_longitud;
+
+    //--------------------------------------------------------------
+    //                          Constructores
+    //--------------------------------------------------------------
+
+    /**
+     * Constructor vacio de la clase.
+     */
+    public EmergenciaDTO() {
+    }
+
+    /**
+     * Constructor con todos los atributos de la clase.
+     * @param fecha
+     * @param estado
+     * @param ubicacion_latitud
+     * @param ubicacion_longitud
+     */
+    public EmergenciaDTO(Date fecha, String estado, int ubicacion_latitud, int ubicacion_longitud) {
+        this.fecha = fecha;
+        this.estado = estado;
+        this.ubicacion_latitud = ubicacion_latitud;
+        this.ubicacion_longitud = ubicacion_longitud;
+    }
 }
